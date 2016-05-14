34f71ac8f4c81c9442b82437ecb9ce6b45ca6f0d
Mike Bostock <mbostock@gmail.com>
2016-05-14 15:29:54 -0700

    Wheel timer.

--- src/zoom.js
+++ src/zoom.js
@@ -2,10 +2,18 @@ import {dispatch} from "d3-dispatch";
 import {event, select} from "d3-selection";
 
 export default function(started) {
+  var wheelTimer,
+      wheelDelay = 150;
+
+  // TODO
+  // var scale = 1,
+  //     translateX = 0,
+  //     translateY = 0;
 
   var listeners = dispatch("start", "zoom", "end")
       .on("start", started);
 
+  // TODO dblclick starts zoom transition
   function zoom(selection) {
     selection
         .on("wheel.zoom", wheeled)
@@ -16,27 +24,37 @@ export default function(started) {
         .style("-webkit-tap-highlight-color", "rgba(0,0,0,0)");
   }
 
+  // TODO don’t dispatch start & end if there is another active gesture (e.g., mousedown)
+  // TODO interrupt transition on this element, if any
+  // TODO dispatch customEvent
+  // TODO update scale and translate based on event.deltaY and event.deltaMode
+  // TODO allow zoom center to be specified, and default to mouse position
   function wheeled() {
-    listeners.apply("zoom", this, arguments);
+    var that = this, args = arguments;
+    if (wheelTimer) clearTimeout(wheelTimer);
+    else listeners.apply("start", that, args);
+    wheelTimer = setTimeout(function() { wheelTimer = null; listeners.apply("end", that, args); }, wheelDelay);
+    listeners.apply("zoom", that, args);
     event.preventDefault();
-    // TODO emit start on first wheel event
-    // TODO emit end if no wheel events seen in 50ms
-    // TODO compute scale delta based on event.deltaY and event.deltaMode
-    // TODO allow zoom center to be specified, and default to mouse position
   }
 
+  // TODO don’t dispatch start & end if there is another active gesture (e.g., wheel)
+  // TODO interrupt transition on this element, if any
+  // TODO dispatch customEvent
+  // TODO update scale and translate
   function mousedowned() {
-    select(event.view).on("mousemove.zoom", mousemoved).on("mouseup.zoom", mouseupped);
-    // TODO
-  }
+    var that = this, args = arguments;
+    listeners.apply("start", that, args);
+    select(event.view).on("mousemove.zoom", mousemoved, true).on("mouseup.zoom", mouseupped, true);
 
-  function mousemoved() {
-    // TODO
-  }
+    function mousemoved() {
+      listeners.apply("zoom", that, args);
+    }
 
-  function mouseupped() {
-    select(event.view).on("mousemove.zoom mouseup.zoom", null);
-    // TODO
+    function mouseupped() {
+      select(event.view).on("mousemove.zoom mouseup.zoom", null);
+      listeners.apply("end", that, args);
+    }
   }
 
   function touchstarted() {
