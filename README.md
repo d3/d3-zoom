e592e54dd88129f119ab2ef71cf0f53e1d7d8608
Mike Bostock <mbostock@gmail.com>
2016-05-16 14:21:33 -0700

    Transitions!

--- README.md
+++ README.md
@@ -7,8 +7,13 @@
 If you use NPM, `npm install d3-zoom`. Otherwise, download the [latest release](https://github.com/d3/d3-zoom/releases/latest). You can also load directly from [d3js.org](https://d3js.org), either as a [standalone library](https://d3js.org/d3-zoom.v0.0.min.js) or as part of [D3 4.0 alpha](https://github.com/mbostock/d3/tree/4). AMD, CommonJS, and vanilla environments are supported. In vanilla, a `d3_zoom` global is exported:
 
 ```html
+<script src="https://d3js.org/d3-color.v0.4.min.js"></script>
 <script src="https://d3js.org/d3-dispatch.v0.4.min.js"></script>
+<script src="https://d3js.org/d3-ease.v0.7.min.js"></script>
+<script src="https://d3js.org/d3-interpolate.v0.7.min.js"></script>
 <script src="https://d3js.org/d3-selection.v0.7.min.js"></script>
+<script src="https://d3js.org/d3-timer.v0.4.min.js"></script>
+<script src="https://d3js.org/d3-transition.v0.2.min.js"></script>
 <script src="https://d3js.org/d3-zoom.v0.0.min.js"></script>
 <script>
 
--- src/zoom.js
+++ src/zoom.js
@@ -1,14 +1,16 @@
 import {dispatch} from "d3-dispatch";
+import {interpolateZoom} from "d3-interpolate";
 import {event, customEvent, select, mouse} from "d3-selection";
+import {transition} from "d3-transition";
 import ZoomEvent from "./event";
 
-// TODO scale and translate must be stored on each selected element, not the behavior
 export default function(started) {
-  var scale = 1,
+  var view = {x: 0, y: 0, k: 1},
+      dx = 960,
+      dy = 500,
       scaleMin = 0,
       scaleMax = Infinity,
-      translateX = 0,
-      translateY = 0,
+      duration = 250,
       zooming = 0,
       wheelTimer,
       wheelDelay = 150,
@@ -22,41 +24,63 @@ export default function(started) {
   var listeners = dispatch("start", "zoom", "end")
       .on("start", started);
 
-  // TODO dblclick starts zoom transition
   function zoom(selection) {
-    selection
-        .on("wheel.zoom", wheeled)
-        .on("mousedown.zoom", mousedowned)
-        .on("touchstart.zoom", touchstarted)
-        .on("touchmove.zoom", touchmoved)
-        .on("touchend.zoom touchcancel.zoom", touchended)
-        .style("-webkit-tap-highlight-color", "rgba(0,0,0,0)");
+    if (selection instanceof transition) {
+      selection
+          .on("start.zoom", function() {
+            emit("start", this, arguments);
+          })
+          .tween("zoom:zoom", function() {
+            var that = this,
+                args = arguments,
+                v = that.__zoom,
+                x = mousePoint ? mousePoint[0] : dx / 2,
+                y = mousePoint ? mousePoint[1] : dy / 2,
+                i = interpolateZoom([(x - v.x) / v.k, (y - v.y) / v.k, dx / v.k], [(x - view.x) / view.k, (y - view.y) / view.k, dx / view.k]);
+            return function(t) {
+              var l = i(t), k = dx / l[2];
+              that.__zoom = {x: x - l[0] * k, y: y - l[1] * k, k: k};
+              emit("zoom", that, args);
+            };
+          })
+          .on("end.zoom", function() {
+            emit("end", this, arguments);
+          });
+    } else {
+      selection
+          .property("__zoom", view)
+          .on("wheel.zoom", wheeled)
+          .on("mousedown.zoom", mousedowned)
+          .on("dblclick.zoom", dblclicked)
+          .on("touchstart.zoom", touchstarted)
+          .on("touchmove.zoom", touchmoved)
+          .on("touchend.zoom touchcancel.zoom", touchended)
+          .style("-webkit-tap-highlight-color", "rgba(0,0,0,0)");
+    }
   }
 
   function point(l) {
-    return [l[0] * scale + translateX, l[1] * scale + translateY];
+    return [l[0] * view.k + view.x, l[1] * view.k + view.y];
   }
 
   function location(p) {
-    return [(p[0] - translateX) / scale, (p[1] - translateY) / scale];
+    return [(p[0] - view.x) / view.k, (p[1] - view.y) / view.k];
   }
 
-  function scaleTo(k) {
-    scale = Math.max(scaleMin, Math.min(scaleMax, k));
+  function scale(k) {
+    view = {k: Math.max(scaleMin, Math.min(scaleMax, k)), x: view.x, y: view.y};
   }
 
-  function translateTo(p, l) {
-    l = point(l);
-    translateX += p[0] - l[0];
-    translateY += p[1] - l[1];
+  function translate(p, l) {
+    l = point(l), view = {k: view.k, x: view.x + p[0] - l[0], y: view.y + p[1] - l[1]};
   }
 
   function emit(type, that, args) {
-    customEvent(new ZoomEvent(type, scale, translateX, translateY), listeners.apply, listeners, [type, that, args]);
+    var view = that.__zoom; // May differ from behavior’s during a transition!
+    customEvent(new ZoomEvent(type, view.k, view.x, view.y), listeners.apply, listeners, [type, that, args]);
   }
 
   // TODO interrupt transition on this element, if any
-  // TODO observe translate extent?
   function wheeled() {
     if (!event.deltaY) return;
 
@@ -68,9 +92,10 @@ export default function(started) {
     wheelTimer = setTimeout(wheelidled, wheelDelay);
     if (start && ++zooming === 1) emit("start", that, args);
 
-    scaleTo(scale * Math.pow(2, -event.deltaY * (event.deltaMode ? 120 : 1) / 500));
-    if (centerPoint) translateTo(centerPoint, centerLocation), mouseLocation = location(mousePoint);
-    else translateTo(mousePoint, mouseLocation);
+    scale(view.k * Math.pow(2, -event.deltaY * (event.deltaMode ? 120 : 1) / 500));
+    if (centerPoint) translate(centerPoint, centerLocation), mouseLocation = location(mousePoint);
+    else translate(mousePoint, mouseLocation);
+    this.__zoom = view;
     emit("zoom", that, args);
 
     function wheelidled() {
@@ -80,7 +105,6 @@ export default function(started) {
   }
 
   // TODO interrupt transition on this element, if any
-  // TODO observe translate extent?
   function mousedowned() {
     var that = this,
         args = arguments;
@@ -90,7 +114,8 @@ export default function(started) {
     if (++zooming === 1) emit("start", that, args);
 
     function mousemoved() {
-      translateTo(mousePoint = mouse(that), mouseLocation);
+      translate(mousePoint = mouse(that), mouseLocation);
+      that.__zoom = view;
       emit("zoom", that, args);
     }
 
@@ -98,6 +123,15 @@ export default function(started) {
       select(event.view).on("mousemove.zoom mouseup.zoom", null);
       if (--zooming === 0) emit("end", that, args);
     }
+  }
+
+  function dblclicked() {
+    var k = Math.log(view.k) / Math.LN2;
+    mouseLocation = location(mousePoint = mouse(this));
+    scale(Math.pow(2, event.shiftKey ? Math.ceil(k) - 1 : Math.floor(k) + 1));
+    translate(mousePoint, mouseLocation);
+    if (duration > 0) select(this).transition().duration(duration).call(zoom);
+    else this.__zoom = view;
   }
 
   function touchstarted() {
@@ -112,22 +146,28 @@ export default function(started) {
     // TODO
   }
 
-  // TODO allow setting
-  zoom.scale = function() {
-    return scale;
+  zoom.scale = function(_) {
+    return arguments.length ? (scale(+_), zoom) : view.k;
   };
 
   zoom.scaleExtent = function(_) {
     return arguments.length ? (scaleMin = +_[0], scaleMax = +_[1], zoom) : [scaleMin, scaleMax];
   };
 
-  // TODO allow setting
-  zoom.translate = function() {
-    return [translateX, translateY];
+  zoom.translate = function(_) {
+    return arguments.length ? (view = {k: view.k, x: +_[0], y: +_[1]}, zoom) : [view.x, view.y];
   };
 
   zoom.center = function(_) {
     return arguments.length ? (centerPoint = _ == null ? null : [+_[0], +_[1]], zoom) : centerPoint;
+  };
+
+  zoom.size = function(_) {
+    return arguments.length ? (dx = +_[0], dy = +_[1], zoom) : [dx, dy];
+  };
+
+  zoom.duration = function(_) {
+    return arguments.length ? (duration = +_, zoom) : duration;
   };
 
   zoom.on = function() {
